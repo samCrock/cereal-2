6141cdf3d3f64d65e18217eb7f9dabed4fdfc0b0
Sam <samuele.lombardi@tutored.it>
2018-05-11 15:20:43 +0200

    Navbar active tabs

--- src/app/common-components/navbar/navbar.component.ts
+++ src/app/common-components/navbar/navbar.component.ts
@@ -4,7 +4,7 @@ import { NavbarService } from '../../services';
 import * as moment from 'moment';
 import { ElectronService } from 'ngx-electron';
 import * as magnet from 'magnet-uri';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
 import { Subject } from 'rxjs/Subject';
 import { Subscription } from 'rxjs/Subscription';
 
@@ -18,20 +18,37 @@ export class NavbarComponent implements OnInit {
 
   private current_show: Object;
   private subscription: Subscription;
+  private enabled = true;
+  private currentRoute = '';
 
   constructor(
     private navbarService: NavbarService,
     private router: Router
-  ) {
+    ) {
   }
 
   ngOnInit() {
     this.navbarService.getShow()
     .subscribe(show => {
-      console.log('Navbar current_show', show);
       this.current_show = show;
     });
-    // this.current_show = this.navbarService.getShow();
+    this.router.events.subscribe(val => {
+      if (val instanceof NavigationEnd) {
+        this.currentRoute = val['url'];
+        if (val['url'] === '/play') {
+          this.enabled = false;
+        } else {
+          this.enabled = true;
+        }
+      }
+    });
+  }
+
+  isActive(route_name) {
+    if (route_name === 'home' && this.currentRoute === '/') { return true; }
+    if (route_name === 'torrents' && this.currentRoute.indexOf('/torrents') > -1) { return true; }
+    if (route_name === 'show' && this.currentRoute.indexOf('/show/') > -1) { return true; }
+    return false;
   }
 
 }
